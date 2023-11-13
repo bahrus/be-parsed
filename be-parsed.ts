d9f4c1e43c309b1e09a3b19a12991194719205e2
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-11-12 21:59:14 -0500

    Initial commit

---
+++ be-parsed.ts
@@ -0,0 +1,37 @@
+import {BE, propDefaults, propInfo} from 'be-enhanced/BE.js';
+import {BEConfig, EnhancementInfo} from 'be-enhanced/types';
+import {XE} from 'xtal-element/XE.js';
+import {Actions, AllProps, AP, PAP, ProPAP, POA} from './types';
+import {register} from 'be-hive/register.js';
+
+export class BeParsed extends BE<AP, Actions, HTMLScriptElement> implements Actions{
+    override async attach(el: HTMLScriptElement, enhancement: EnhancementInfo) {
+        this.value = JSON.parse(el.innerHTML);
+        this.resolved = true;
+    }
+
+}
+
+export interface BeParsed extends AllProps{}
+
+const tagName = 'be-parsed';
+const ifWantsToBe = 'parsed';
+const upgrade = '*';
+
+const xe = new XE<AP, Actions>({
+    config: {
+        tagName,
+        isEnh: true,
+        propDefaults: {
+            ...propDefaults
+        },
+        propInfo: {
+            ...propInfo,
+
+        },
+        actions:{}
+    },
+    superclass: BeParsed
+});
+
+register(ifWantsToBe, upgrade, tagName);
--- types.d.ts
+++ types.d.ts
@@ -2,9 +2,11 @@ import { ActionOnEventConfigs } from "trans-render/froop/types";
 import {IBE} from 'be-enhanced/types';
 
 export interface EndUserProps extends IBE<HTMLScriptElement>{
-
+    
 }
-export interface AllProps extends EndUserProps{}
+export interface AllProps extends EndUserProps{
+    value: any,
+}
 
 export type AP = AllProps;
 
@@ -15,5 +17,5 @@ export type ProPAP = Promise<PAP>;
 export type POA = [PAP | undefined, ActionOnEventConfigs<PAP, Actions>];
 
 export interface Actions{
-    
+
 }
