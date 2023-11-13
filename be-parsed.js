f88392a5a7e646cc4f96b36fcef807cb7200c102
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-11-12 22:03:05 -0500

    Initial commit

---
+++ be-parsed.js
@@ -0,0 +1,27 @@
+import { BE, propDefaults, propInfo } from 'be-enhanced/BE.js';
+import { XE } from 'xtal-element/XE.js';
+import { register } from 'be-hive/register.js';
+export class BeParsed extends BE {
+    async attach(el, enhancement) {
+        this.value = JSON.parse(el.innerHTML);
+        this.resolved = true;
+    }
+}
+const tagName = 'be-parsed';
+const ifWantsToBe = 'parsed';
+const upgrade = '*';
+const xe = new XE({
+    config: {
+        tagName,
+        isEnh: true,
+        propDefaults: {
+            ...propDefaults
+        },
+        propInfo: {
+            ...propInfo,
+        },
+        actions: {}
+    },
+    superclass: BeParsed
+});
+register(ifWantsToBe, upgrade, tagName);
