093282ec7ee22f0f15c2bcae61b9a203c84589f6
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-11-12 21:15:01 -0500

    Initial commit

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+import { ActionOnEventConfigs } from "trans-render/froop/types";
+import {IBE} from 'be-enhanced/types';
+
+export interface EndUserProps extends IBE<HTMLScriptElement>{
+
+}
+export interface AllProps extends EndUserProps{}
+
+export type AP = AllProps;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP = Promise<PAP>;
+
+export type POA = [PAP | undefined, ActionOnEventConfigs<PAP, Actions>];
+
+export interface Actions{
+    
+}
